be600f85dd1a6b76ab16c13dc79aedad1118fecf
matías ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2021-12-02 13:20:34 +0000

    geo ok

--- maths.py
+++ maths.py
@@ -1,12 +1,18 @@
-# geome series  
-# evaluate:  3⋅(−2)**n−1
+#--- geome series  
+#--- evaluate:  3⋅(−2)**n−1
+#--- (You are finding S10 for the series 3−6+12−24+... , 
+#--- whose common ratio is −2 .)
+#--- Sn=a1(1−rn)/1−r
 
+
+sn = 0
 print("hallo")
-for q in range(1,12):
-  g = 3*((-2)**(q-1))
+for q in range(1,11):
+  g = 3*(-2)**(q-1)
   gg = (-2)**(0)
   
-  
-  print(g, q)
-ggg = (-2)**(8)
+  sn +=  g
+  print(g, gg)
+ggg = (-2)**9
 print(ggg, "ggg")
+print(sn)
