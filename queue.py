213d967076f0b246dde664ed231879156a60d1e3
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2021-12-18 04:54:21 +0000

    futures

--- queue.py
+++ queue.py
@@ -14,7 +14,13 @@ def test_q(name):
   ret = "hello " + name 
   return ret
 
-
+  def pooled():
+    workers = 20
+    ret = []
+    with ThreadPoolExecutor(max_qierkers=qorkers) as ex:
+      for x in range(workers):
+        v = random.randrange(1,5)
+        future =ex.submit()
 
 def main():
   logging.basicConfig(format="%(levelname)s - %(asctime)s.%(msecs)03d: %(message)")
