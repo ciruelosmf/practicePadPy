8d558cc4615d7cc393674c4cad7031719dc69c17
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2021-12-18 04:57:51 +0000

    f

--- queue.py
+++ queue.py
@@ -10,17 +10,21 @@ from queue import Queue
 
 def test_q(name):
   threadname = threading.current_thread().name
-  logging.info(f"start: {threadnmae})
+  logging.info(f"start: {threadname}")
   ret = "hello " + name 
   return ret
 
   def pooled():
     workers = 20
     ret = []
-    with ThreadPoolExecutor(max_qierkers=qorkers) as ex:
+    with ThreadPoolExecutor(max_workers=workers) as ex:
       for x in range(workers):
         v = random.randrange(1,5)
-        future =ex.submit()
+        future =ex.submit(test_q, "B")
+        ret.append(future)
+      logging.info("hola")
+      for r in ret:
+        logging.info(r.result())
 
 def main():
   logging.basicConfig(format="%(levelname)s - %(asctime)s.%(msecs)03d: %(message)")
