0d8c48b900edbb56f3da7466cf69afdb136e33a6
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2021-12-23 04:30:30 +0000

    plot

--- calculus.py
+++ calculus.py
@@ -1,13 +1,37 @@
-def f(x):
+def aa(x):
   return x**2
 
 print(f(3))
 
 
-def derivative(f,x):
-    """
-    Returns the value of the derivative of
-    the function at a given x-value.
-    """
-    delta_x = 1/1000000
-    return (f(x+delta_x) - f(x))/delta_x
+def deriv(aa,x):
+
+  deltax=(1/1000000)
+  return (aa(x+deltax) - aa(x))/deltax
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 100 linearly spaced numbers
+x = np.linspace(0,1,10)
+eta1=0.05
+eta2=0.1
+lmd1= 2.8
+# the function, which is y = x^2 here
+y = 0.5*(-(1/3)*eta1*x**2 + np.sqrt( ((1/3)*eta1*x**2)**2-4*(3*x**2*2.8**2-1)) 
+
+# setting the axes at the centre
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+# plot the function
+plt.plot(x,y, 'r')
+
+# show the plot
+plt.show()
